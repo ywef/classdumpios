1cff167332cb548e5037cfff1f37d2953dd84646
Kevin Bradley <kevin.w.bradley@me.com>
2023-04-27 12:30:55 -0700

    added build target for tvos lib as well

--- build_debug.sh
+++ build_debug.sh
@@ -6,12 +6,14 @@ if [ -z $XCP ]; then
     xcodebuild -target classdumpc -configuration Debug
     xcodebuild -target classdump -configuration Debug
     xcodebuild -target classdump-ios -configuration Debug
+    xcodebuild -target classdump-tvos -configuration Debug
     xcodebuild -target classdumpc-ios -configuration Debug
     xcodebuild -target classdumpc-tvos -configuration Debug
 else
     xcodebuild -target classdumpc -configuration Debug | $XCP
     xcodebuild -target classdump -configuration Debug | $XCP
     xcodebuild -target classdump-ios -configuration Debug | $XCP
+    xcodebuild -target classdump-tvos -configuration Debug | $XCP
     xcodebuild -target classdumpc-ios -configuration Debug | $XCP
     xcodebuild -target classdumpc-tvos -configuration Debug | $XCP
 fi
--- build_release.sh
+++ build_release.sh
@@ -5,6 +5,7 @@ XCP=$(/usr/bin/which xcpretty)
 if [ -z $XCP ]; then
     xcodebuild -target classdumpc -configuration Release
     xcodebuild -target classdump -configuration Release
+    xcodebuild -target classdump-tvos -configuration Release
     xcodebuild -target classdump-ios -configuration Release
     xcodebuild -target classdumpc-ios -configuration Release
     xcodebuild -target classdumpc-tvos -configuration Release
@@ -12,6 +13,7 @@ else
     xcodebuild -target classdumpc -configuration Release | $XCP
     xcodebuild -target classdump -configuration Release | $XCP
     xcodebuild -target classdump-ios -configuration Release | $XCP
+    xcodebuild -target classdump-tvos -configuration Release | $XCP
     xcodebuild -target classdumpc-ios -configuration Release | $XCP
     xcodebuild -target classdumpc-tvos -configuration Release | $XCP
 fi
